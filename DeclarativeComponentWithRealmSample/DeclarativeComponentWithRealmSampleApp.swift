2df7333ea373cb9b7552bf2138830a7fbe99272e
Fumiya Sakai <just1factory@gmail.com>
2022-06-20 23:34:12 +0900

    Initial Commit

---
+++ DeclarativeComponentWithRealmSample/DeclarativeComponentWithRealmSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeclarativeComponentWithRealmSampleApp.swift
+//  DeclarativeComponentWithRealmSample
+//
+//  Created by 酒井文也 on 2022/06/19.
+//
+
+import SwiftUI
+
+@main
+struct DeclarativeComponentWithRealmSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
