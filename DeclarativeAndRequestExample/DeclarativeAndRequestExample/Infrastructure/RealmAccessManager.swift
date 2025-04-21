9a6e6820bcaf2dcd367f556688b553394fdbeb9a
fumiyasac <just1factory@gmail.com>
2025-04-22 07:26:07 +0900

    ライブラリの整理

--- DeclarativeAndRequestExample/DeclarativeAndRequestExample/Infrastructure/RealmAccessManager.swift
+++ DeclarativeAndRequestExample/DeclarativeAndRequestExample/Infrastructure/RealmAccessManager.swift
@@ -5,24 +5,24 @@
 //  Created by 酒井文也 on 2023/05/09.
 //
 
-import Foundation
-import RealmSwift
-
-// MARK: - Protocol
-
-protocol RealmAccessProtocol {}
-
-final class RealmAccessManager {
-
-    // MARK: - Singleton Instance
-
-    static let shared = RealmAccessManager()
-
-    // MARK: - Properies
-
-    private let schemaConfig = Realm.Configuration(schemaVersion: 0)
-}
-
-// MARK: - RealmAccessProtocol
-
-extension RealmAccessManager: RealmAccessProtocol {}
+//import Foundation
+////import RealmSwift
+//
+//// MARK: - Protocol
+//
+//protocol RealmAccessProtocol {}
+//
+//final class RealmAccessManager {
+//
+//    // MARK: - Singleton Instance
+//
+//    static let shared = RealmAccessManager()
+//
+//    // MARK: - Properies
+//
+//    private let schemaConfig = Realm.Configuration(schemaVersion: 0)
+//}
+//
+//// MARK: - RealmAccessProtocol
+//
+//extension RealmAccessManager: RealmAccessProtocol {}
