7817944413be56af612ce95dd2a4d905d5183c07
fumiyasac <just1factory@gmail.com>
2025-04-26 12:25:35 +0900

    UIColorExtensionの新規追加

--- DeclarativeAndRequestExample/DeclarativeAndRequestExample/App/AppDelegate.swift
+++ DeclarativeAndRequestExample/DeclarativeAndRequestExample/App/AppDelegate.swift
@@ -13,12 +13,42 @@ final class AppDelegate: NSObject, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
-        // TODO: UIKit側におけるデザイン調整用の追加処理
+        // MEMO: UIKit側におけるデザイン調整用の追加処理
+        setupNavigationAppearnces()
+        setupTabBarAppearances()
 
         return true
     }
 
     // MARK: - Private Function
 
-    
+    private func setupNavigationAppearnces() {
+
+        // MEMO: NavigationBarのタイトル色を黒色に合わせる対応
+        var titleTextAttributes: [NSAttributedString.Key : Any] = [:]
+        titleTextAttributes[NSAttributedString.Key.font] = UIFont(name: "HelveticaNeue-Bold", size: 15.0)!
+        titleTextAttributes[NSAttributedString.Key.foregroundColor] = UIColor.black
+        let newNavigationAppearance = UINavigationBarAppearance()
+        newNavigationAppearance.configureWithTransparentBackground()
+        newNavigationAppearance.backgroundColor = UIColor(code: "eeeeee")
+        newNavigationAppearance.titleTextAttributes = titleTextAttributes
+        UINavigationBar.appearance().standardAppearance = newNavigationAppearance
+        UINavigationBar.appearance().scrollEdgeAppearance = newNavigationAppearance
+    }
+
+    private func setupTabBarAppearances() {
+
+        // MEMO: UITabBarItemの選択時と非選択時の文字色の装飾設定
+        let tabBarAppearance = UITabBarAppearance()
+        let tabBarItemAppearance = UITabBarItemAppearance()
+        tabBarItemAppearance.normal.titleTextAttributes = [
+            NSAttributedString.Key.foregroundColor : UIColor.lightGray
+        ]
+        tabBarItemAppearance.selected.titleTextAttributes = [
+            NSAttributedString.Key.foregroundColor : UIColor.systemBlue
+        ]
+        tabBarAppearance.stackedLayoutAppearance = tabBarItemAppearance
+        UITabBar.appearance().standardAppearance = tabBarAppearance
+        UITabBar.appearance().scrollEdgeAppearance = tabBarAppearance
+    }
 }
